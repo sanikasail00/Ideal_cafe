8cfcb52dbd8d9dba8ae39f13a6dd262f0beb7158
Priyadarshini <priyadarshinimulloli@gmail.com>
2025-04-09 10:00:00 +0530

    Added navbar

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
-import { Container, Navbar, Form, NavDropdown, Button, Nav } from "react-bootstrap";
+import React from "react";
+import {
+  Container,
+  Navbar,
+  Form,
+  NavDropdown,
+  Button,
+  Nav,
+} from "react-bootstrap";
 
 export default function NavigationBar() {
   return (
     <div>
-        <Navbar expand="lg" className="bg-light">
-            <Container>
+      <Navbar expand="lg" className="bg-light">
+        <Container>
+          
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
             <Form className="d-flex flex-grow-1" style={{ maxWidth: "900px" }}>
               <Form.Control
                 type="search"
@@ -14,10 +24,17 @@ export default function NavigationBar() {
                 style={{ width: "100%" }}
               />
             </Form>
-            </Container>
-
-        </Navbar>
-      
+            <Nav className="d-flex flex-wrap">
+               <Nav.Link >Home</Nav.Link>
+               <Nav.Link >Menu</Nav.Link>
+               <Nav.Link >Offers</Nav.Link>
+               <Nav.Link >Location</Nav.Link>
+               <Nav.Link >Contact Us</Nav.Link>
+               <Nav.Link >Sign In</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
-  )
+  );
 }
