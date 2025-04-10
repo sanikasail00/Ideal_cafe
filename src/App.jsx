ffd892c6247908b8962afa0a2969ab3fa96b347a
Deepthi <deepthiacharya2005@gmail.com>
2025-04-10 21:48:41 +0530

    changes to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import NavigationBar from './components/NavigationBar'
 import Home from './components/Home'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 export default function App() {
   return (
     <div>
